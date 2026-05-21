5d37f71b147a3ff7264752a5061b75a915b0a414
kavyavenkatesh-10027 <kavyavenkatesh2007@gmail.com>
2026-05-21 15:15:45 +0530

    Commit message

--- src/model/Slot.java
+++ src/model/Slot.java
@@ -1,23 +1,28 @@
 package model;
 
 import util.Generator;
-
 import java.util.Collections;
 import java.util.Map;
 
 public class Slot {
     private final String slotId;
+    private final String vendingMachineId;
     //has-a relationship(Slot has food items)
     private final Map<String, Integer> foodItemsInSlot;
     //This is a map for foodId and quantity that are present in that particular slot
 
-    public Slot(Map<String, Integer> foodItemsInSlot){
+    public Slot(String vendingMachineId, Map<String, Integer> foodItemsInSlot){
         this.slotId = Generator.generateSlotId();
+        this.vendingMachineId = vendingMachineId;
         this.foodItemsInSlot = foodItemsInSlot;
     }
 
     public String getSlotId() {
         return slotId;
+    }
+
+    public String getVendingMachineId() {
+        return vendingMachineId;
     }
 
     public Map<String, Integer> getFoodItemsInSlot() {
