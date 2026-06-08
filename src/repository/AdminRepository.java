b4517272c0c687411914c6bbc0f140384be74565
kavyavenkatesh-10027 <kavyavenkatesh2007@gmail.com>
2026-06-08 10:55:34 +0530

    Commit message

--- src/repository/AdminRepository.java
+++ src/repository/AdminRepository.java
@@ -1,7 +1,6 @@
 package repository;
 
 import model.Admin;
-import util.VendingMachineException;
 
 public class AdminRepository extends BaseRepository<Admin> {
 
@@ -19,44 +18,3 @@ public class AdminRepository extends BaseRepository<Admin> {
         return admin.getAdminId();
     }
 }
-//import model.Admin;
-//import util.VendingMachineException;
-//
-//import java.util.*;
-//
-//public class AdminRepository {
-//
-//    private static AdminRepository instance;
-//    private final Map<String, Admin> admins = new HashMap<>();
-//
-//    private AdminRepository() {}
-//
-//    public static AdminRepository getInstance() {
-//        if (instance == null) {
-//            instance = new AdminRepository();
-//        }
-//        return instance;
-//    }
-//
-//    public void add(Admin admin) {
-//        if (admin == null) {
-//            throw new VendingMachineException("Admin cannot be null.");
-//        }
-//        admins.put(admin.getAdminId(), admin);
-//    }
-//
-//    public Admin findById(String adminId) {
-//        if (adminId == null) {
-//            throw new VendingMachineException("Admin ID cannot be null");
-//        }
-//        return admins.get(adminId);
-//    }
-//
-//    public List<Admin> findAll() {
-//        return Collections.unmodifiableList(new ArrayList<>(admins.values()));
-//    }
-//
-//    public boolean removeById(String adminId) {
-//        return admins.remove(adminId) != null;
-//    }
-//}
--- src/repository/VendingMachineRepository.java
+++ src/repository/VendingMachineRepository.java
@@ -1,7 +1,6 @@
 package repository;
 
 import model.VendingMachine;
-import util.VendingMachineException;
 
 public class VendingMachineRepository extends BaseRepository<VendingMachine> {
 
@@ -19,45 +18,3 @@ public class VendingMachineRepository extends BaseRepository<VendingMachine> {
         return vm.getVendingMachineId();
     }
 }
-//import model.VendingMachine;
-//import util.VendingMachineException;
-//
-//import java.util.*;
-//
-//public class VendingMachineRepository {
-//
-//    private static VendingMachineRepository instance;
-//    private final Map<String, VendingMachine> vendingMachines = new HashMap<>();
-//
-//    private VendingMachineRepository() {}
-//
-//    public static VendingMachineRepository getInstance() {
-//        if (instance == null) {
-//            instance = new VendingMachineRepository();
-//        }
-//        return instance;
-//    }
-//
-//    public void add(VendingMachine vm) {
-//        if (vm == null) {
-//            throw new VendingMachineException("VendingMachine cannot be null.");
-//        }
-//        vendingMachines.put(vm.getVendingMachineId(), vm);
-//    }
-//
-//    public VendingMachine findById(String vendingMachineId) {
-//        return vendingMachines.get(vendingMachineId);
-//    }
-//
-//    public List<VendingMachine> findAll() {
-//        return Collections.unmodifiableList(new ArrayList<>(vendingMachines.values()));
-//    }
-//
-//    public void removeById(String vendingMachineId) {
-//        vendingMachines.remove(vendingMachineId);
-//    }
-//
-//    public boolean existsById(String vendingMachineId) {
-//        return vendingMachines.containsKey(vendingMachineId);
-//    }
-//}
